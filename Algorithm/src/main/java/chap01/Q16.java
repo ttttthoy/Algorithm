2e44f92dec0c93fd1b31c8d85b1d60f8bb892e25
ttttthoy <tremexplo8@daum.net>
2022-02-02 23:03:15 +0900

    triangleStar

--- Algorithm/src/main/java/chap01/Q16.java
+++ Algorithm/src/main/java/chap01/Q16.java
@@ -1,9 +1,29 @@
 package chap01;
 
+import java.util.Scanner;
+
 public class Q16 {
     static void spira(int n){
         for (int i = 1; i <= n; i++){
-            for (int j = 1; j)
+            for (int j = 1; j <= n - i + 1; j ++)
+                System.out.print(" ");
+            for (int j = 1; j <= (i - 1) * 2 + 1; j ++)
+                System.out.print("*");
+                System.out.println();
+
         }
+    }
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int n;
+
+        do {
+            System.out.print("몇 차 이등변삼각형 인가요? : ");
+            n = sc.nextInt();
+        } while (n <= 0);
+
+        spira(n);
     }
 }
