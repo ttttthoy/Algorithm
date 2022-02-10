2ee89b5bdd8fd9e6fa3e4493f5a395c83950a782
ttttthoy <tremexplo8@daum.net>
2022-02-10 15:18:38 +0900

    digit2

--- Algorithm/src/main/java/chap02/CardConvRev.java
+++ Algorithm/src/main/java/chap02/CardConvRev.java
@@ -14,4 +14,35 @@ public class CardConvRev {
 
         return digits;
     }
+
+    public static void main(String[] args){
+        Scanner snn = new Scanner(System.in);
+        int no;
+        int cd;
+        int dno;
+        int retry;
+        char[] cno = new char[2];
+
+        System.out.println("10진수를 기수 변환합니다.");
+        do {
+            do{
+                System.out.print("변환하는 음이 아닌 정수 : ");
+                no = snn.nextInt();
+            } while (no < 0);
+
+            do{
+                System.out.print("어떤 진수로 변환할까요? (2~36) : ");
+                cd = snn.nextInt();
+            } while (cd < 2 || cd > 36);
+            dno = cardConvR(no, cd, cno);
+
+            System.out.print( cd +  "진수로는 ");
+            for (int i = dno -1 ; i >= 0; i--)
+                System.out.print(cno[i]);
+            System.out.println("입니다.");
+
+            System.out.print("한 번 더 할까요? (1.예/0.아니오) : ");
+            retry = snn.nextInt();
+        } while (retry == 1);
+    }
 }
