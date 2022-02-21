cafbeeadbfd1b0476deb925865ae0a29bf2961d8
ttttthoy <tremexplo8@daum.net>
2022-02-22 04:35:50 +0900

    stream

---
+++ baekjoon/Main_1000.java
@@ -0,0 +1,13 @@
+package baekjoon;
+import java.util.Scanner;
+
+public class Main_1000 {
+    public static void main(String[] args) {
+        Scanner sn = new Scanner(System.in);
+
+        int a = sn.nextInt();
+        int b = sn.nextInt();
+
+        System.out.println( a+b );
+    }
+}
---
+++ baekjoon/Main_10869.java
@@ -0,0 +1,17 @@
+package baekjoon;
+import java.util.Scanner;
+
+public class Main_10869 {
+    public static void main(String[] args){
+        Scanner sn = new Scanner(System.in);
+
+        int a = sn.nextInt();
+        int b = sn.nextInt();
+
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+    }
+}
