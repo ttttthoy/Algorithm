95be28be2acde6ea070e4c29e988157c64171b43
ttttthoy <tremexplo8@daum.net>
2022-02-22 03:18:31 +0900

    2557

---
+++ baekjoon/Main_2557.java
@@ -0,0 +1,16 @@
+package baekjoon;
+
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
+public class Main_2557 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedWriter buff = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        buff.write("Hello World!");
+        buff.flush();
+        buff.close();
+    }
+}
