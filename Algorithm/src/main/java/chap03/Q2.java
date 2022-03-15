eea7e73dd42bc4ca2621c6f68bcdbbaf403d729c
ttttthoy <tremexplo8@daum.net>
2022-03-15 23:42:19 +0900

    percent2

--- Algorithm/src/main/java/chap03/Q2.java
+++ Algorithm/src/main/java/chap03/Q2.java
@@ -2,7 +2,6 @@ package chap03;
 import java.util.Scanner;
 
 public class Q2{
-
     static int seqSearchEx(int[] a, int n, int key) {
         System.out.print("   |");
 
@@ -33,5 +32,28 @@ public class Q2{
         return -1;
     }
 
-}
+        public static void main(String[] args) {
+            Scanner stdIn = new Scanner(System.in);
+
+            System.out.print("요솟수： ");
+            int num = stdIn.nextInt();
+            int[] x = new int[num];
+
+            for (int i = 0; i < num; i++) {
+                System.out.print("x[" + i + "]： ");
+                x[i] = stdIn.nextInt();
+            }
+
+            System.out.print("찾는 값： ");
+            int ky = stdIn.nextInt();
+
+            int idx = seqSearchEx(x, num, ky);
+
+            if (idx == -1)
+                System.out.println("그 값의 요소가 없습니다.");
+            else
+                System.out.println(ky + "은 " + "x[" + idx + "]에 있습니다.");
+        }
+    }
+
 
