961830e3359b841b5ff661e3c2d5333dd805da64
ttttthoy <tremexplo8@daum.net>
2022-03-25 22:54:28 +0900

    ascii

---
+++ baekjoon/Main_11654.java
@@ -0,0 +1,11 @@
+package baekjoon;
+
+import java.io.IOException;
+
+public class Main_11654 {
+    public static void main(String[] args) throws IOException {
+
+        int a = System.in.read();
+        System.out.print(a);
+    }
+}
