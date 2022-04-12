5daa3d1498826f04658c4da35a336d98652b7b53
ttttthoy <tremexplo8@daum.net>
2022-04-12 19:57:43 +0900

    queadd

--- Algorithm/src/main/java/chap04/IntAryQueue.java
+++ Algorithm/src/main/java/chap04/IntAryQueue.java
@@ -14,19 +14,75 @@ public class IntAryQueue {
 
     private int max;
     private int num;
+    private int[] que;
 
     public IntAryQueue(int capacity) {
         num = 0;
         max = capacity;
+        try {
+            que = new int[max];
+        } catch (OutOfMemoryError e) {
+            max = 0;
+        }
+    }
 
+    public int enque(int x) throws OverflowIntAryQueueException {
+        if (num >= max)
+            throw new OverflowIntAryQueueException(); // 큐가 가득 참
+        que[num++] = x;
+        return x;
+    }
+
+    public int deque() throws EmptyIntAryQueueException {
+        if (num <= 0)
+            throw new EmptyIntAryQueueException(); // 큐가 비어 있음
+        int x = que[0];
+        for (int i = 0; i < num - 1; i++)
+            que[i] = que[i + 1];
+        num--;
+        return x;
+    }
+
+    public int peek() throws EmptyIntAryQueueException {
+        if (num <= 0)
+            throw new EmptyIntAryQueueException(); // 큐가 비어 있음
+        return que[num - 1];
+    }
+
+    public int indexOf(int x) {
+        for (int i = 0; i < num; i++)
+            if (que[i] == x)
+                return i;
+        return -1;
     }
 
     public void clear() {
         num = 0;
     }
 
-    //큐 용량 반환
     public int capacity() {
         return max;
+    }
+
+    public int size() {
+        return num;
+    }
+
+    public boolean isEmpty() {
+        return num <= 0;
+    }
+
+    public boolean isFull() {
+        return num >= max;
+    }
+
+    public void dump() {
+        if (num <= 0)
+            System.out.println("큐가 비었습니다.");
+        else {
+            for (int i = 0; i < num; i++)
+                System.out.print(que[i] + " ");
+            System.out.println();
+        }
     }
 }
