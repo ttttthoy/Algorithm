67f7f7d9d9772759e084e1ed4339b86b2ede6f2f
ttttthoy <tremexplo8@daum.net>
2022-04-16 19:10:13 +0900

    10951

---
+++ baekjoon/Main_10951.java
@@ -0,0 +1,19 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Main_10951 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        while (sc.hasNext()) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+
+            int result = a + b;
+            System.out.println(result);
+        }
+    }
+}
