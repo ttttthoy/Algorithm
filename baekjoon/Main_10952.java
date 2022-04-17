a20c36919244645631e14f40de6aa84bea3f7bd2
ttttthoy <tremexplo8@daum.net>
2022-04-17 20:58:02 +0900

    10952

---
+++ baekjoon/Main_10952.java
@@ -0,0 +1,29 @@
+package baekjoon;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.io.IOException;
+
+public class Main_10952 {
+    public static void main(String[] args) throws IOException{
+
+        BufferedReader buff = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        while(true) {
+
+            st = new StringTokenizer(buff.readLine()," ");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            if(a == 0 && b == 0) {
+                break;
+            }
+            sb.append((a+b)).append('\n');
+        }
+        System.out.println(sb);
+    }
+}
+
