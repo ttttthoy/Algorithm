ed8d96962153b7272364811c52d99cbb4de8106e
ttttthoy <tremexplo8@daum.net>
2022-04-20 17:41:32 +0900

    11718

---
+++ baekjoon/Main_11718.java
@@ -0,0 +1,17 @@
+package baekjoon;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main_11718 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader buff = new BufferedReader(new InputStreamReader(System.in));
+
+        String strIn;
+
+        while ((strIn=buff.readLine()) != null) {
+            System.out.println(strIn);
+        }
+    }
+}
